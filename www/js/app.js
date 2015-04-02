ce66dfac56a3d197b3a7972b1e834f29dda5209f
Muhammad Zubair <fly_zubair@hotmail.com>
2015-04-02 19:53:45 +0500

    Modal working

--- www/js/app.js
+++ www/js/app.js
@@ -18,7 +18,7 @@ angular.module('starter', ['ionic'])
             }
         });
     })
-    .controller('videoController', function ($scope,$ionicPopover) {
+    .controller('videoController', function ($scope,$ionicPopover,$ionicModal) {
 
         /* ++++++++++++++++++ Comment 26th March, 2015 ++++++++++++++++++++++
 
@@ -100,7 +100,25 @@ angular.module('starter', ['ionic'])
 
         };
 
-      
+        $ionicModal.fromTemplateUrl('my-modal.html', {
+            scope: $scope,
+            animation: 'slide-in-up'
+        }).then(function(modal) {
+            $scope.modal = modal;
+        });
+
+
+        $scope.showModal= function () {
+
+            $scope.modal.show();
+        };
+
+
+
+
+        $scope.closeModal = function() {
+            $scope.modal.hide();
+        };
 
 
 
